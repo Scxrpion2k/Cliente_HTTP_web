bad48693874e5a9abb4b62bd3621c72e0bf36675
Scxrpion2k <wilmerbladimir11@gmail.com>
2026-06-01 16:06:02 -0400

    Funcion tipo de archivo

--- src/main/java/edu/puccm/eict/ClienteHTTP/Main.java
+++ src/main/java/edu/puccm/eict/ClienteHTTP/Main.java
@@ -1,6 +1,10 @@
 package edu.puccm.eict.ClienteHTTP;
 
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -9,9 +13,10 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String url;
+        String tipo;
         do {
             IO.println("Introduce una URL: ");
             url = scanner.nextLine().trim();
@@ -20,9 +25,21 @@ public class Main {
             if(!esValida(url)){
                 IO.println("URL invalida");
             }
+
+            Connection.Response response = Jsoup.connect(url).ignoreContentType(true).execute();
+            tipo = response.contentType();
+            IO.println("El tipo es: "+tipo);
+
+            if(tipo != null && tipo.startsWith("text/html")){
+
+                return;
+            };
+
+
         } while (!esValida(url));
 
         scanner.close();
+
 
     }
 
